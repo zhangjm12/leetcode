20137b3e9d046ca249c1a0e9dbe2a43a5d4bd83a
JM Zhang <zhangjm12@lenovo.com>
2021-05-04 10:01:07 +0800

    leetcode rub - py

---
+++ rub-py/rub.py
@@ -0,0 +1,46 @@
+from typing import List
+
+def rub(nums:List[int])->int:
+    n = len(nums)
+    r = [0] * n
+
+    if n == 0:
+        return 0
+    if n == 1:
+        return nums[0]
+
+    r[0] = nums[0]
+    r[1] = max(nums[0], nums[1])
+
+    for i in range(2, n):
+        r[i] = max(nums[i] + r[i-2], r[i-1])
+    
+    return r[n-1]
+
+
+def rub1(nums: List[int]) -> int:
+    n = len(nums)
+
+    if n == 1:
+        return nums[0]
+    if n == 2:
+        return max(nums[0], nums[1])
+
+    return max(rub_range(nums, 0, n - 2), rub_range(nums, 1, n - 1))
+
+
+def rub_range(nums: List[int], start: int, end: int) -> int:
+    r = [0] * (end - start + 1)
+
+    r[0] = nums[start]
+    r[1] = nums[start+1]
+    for i in range(2, end-start+1):
+        r[i] = max(nums[start+i] + r[i-2], r[i-1])
+
+    return r[end-start]
+
+
+if __name__ == '__main__':
+    test = [3, 1, 2, 5, 3, 1, 4, 11]
+    print(rub(test))
+    print(rub1(test))
